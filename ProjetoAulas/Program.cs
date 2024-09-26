57df80a3e2d68559b618177eb6cbb7cc7d976226
1KA11e1 <kaile030611@gmail.com>
2024-09-26 11:06:19 -0300

    initial commit

---
+++ ProjetoAulas/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("primeiro programa");
+
+int idade = 33;
+Console.WriteLine(idade);
+
+string nomePessoa = "Alerrandro";
+Console.WriteLine(nomePessoa);
+
+decimal valor = 200.99m;
+Console.WriteLine(valor);
+double valorDouble = 200.99;
+Console.WriteLine(valorDouble);
+float valorFloat = 200.99f;
+Console.WriteLine(valorFloat);
+
+var idadeNova = 21;
+Console.WriteLine(idadeNova);
+
+char flag = 'Y';
+Console.WriteLine(flag);
+
+bool ativo = true;
+ativo = false;
+Console.WriteLine(ativo);
+
+//isso é uma constante
+const string descricao = "Curso CSHARP";
+//descricao = "curso";
+
+Console.WriteLine(descricao);
+*/
+int numero1 = 1;
+var numero2 = 2;
+/*
+int soma = numero1 + numero2;
+Console.WriteLine(soma);
+
+int subtracao = numero2 - numero1;
+Console.WriteLine(subtracao);
+
+int multiplicacao = (numero2 * numero1)*10;
+Console.WriteLine(multiplicacao);
+
+int divisao = numero2 / numero1;
+Console.WriteLine(divisao);
+
+bool igual = numero1 == numero2;
+Console.WriteLine(igual);
+
+bool maior = numero2 > numero1;
+Console.WriteLine(maior);
+
+bool menor = numero2 < numero1;
+Console.WriteLine(menor);
+
+bool menorOuIgual = numero2 <= numero1;
+Console.WriteLine(menorOuIgual);
+
+bool maiorOuIgual = numero2 >= numero1;
+Console.WriteLine(maiorOuIgual);
+
+bool diferente = numero2 != numero1;
+Console.WriteLine(diferente);
+*/
+
