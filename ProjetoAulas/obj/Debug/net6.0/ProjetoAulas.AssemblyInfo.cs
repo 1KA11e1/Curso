09a24fecb575b93636bf5bd1a4fbd55b9393f3ca
1KA11e1 <kaile030611@gmail.com>
2024-10-02 09:29:33 -0300

    aa

--- ProjetoAulas/obj/Debug/net6.0/ProjetoAulas.AssemblyInfo.cs
+++ ProjetoAulas/obj/Debug/net6.0/ProjetoAulas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoAulas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+937c3d42fd75c89dbeaca481ae4785ac3b95fca6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+177271f538645312d80d4d6e2090034aaf6b7755")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoAulas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoAulas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
